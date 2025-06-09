9a9b3d042b88a82695d90f16eb9983edbc79e4e3
GenkiYasui <genki.yasui.a722@dirbato.co.jp>
2025-06-09 11:55:45 +0900

    ログインアクションの作成

--- daily_report_system/src/main/java/actions/AuthAction.java
+++ daily_report_system/src/main/java/actions/AuthAction.java
@@ -6,6 +6,9 @@ import javax.servlet.ServletException;
 import constants.AttributeConst;
 import constants.ForwardConst;
 import services.EmployeeService;
+import actions.views.EmployeeView; 
+import constants.MessageConst;
+import constants.PropertyConst;
 
 /**
  * 認証に関する処理を行うActionクラス
@@ -47,5 +50,48 @@ public class AuthAction extends ActionBase {
 
         // ログイン画面を表示
         forward(ForwardConst.FW_LOGIN);
+    }
+    /**
+     * ログイン処理を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void login() throws ServletException, IOException {
+
+        String code = getRequestParam(AttributeConst.EMP_CODE);
+        String plainPass = getRequestParam(AttributeConst.EMP_PASS);
+        String pepper = getContextScope(PropertyConst.PEPPER);
+
+        // 有効な従業員か認証する
+        Boolean isValidEmployee = service.validateLogin(code, plainPass, pepper);
+
+        if (isValidEmployee) {
+            // 認証成功の場合
+
+            // CSRF対策 tokenのチェック
+            if (checkToken()) {
+
+                // ログインした従業員情報を取得
+                EmployeeView ev = service.findOne(code, plainPass, pepper);
+                // セッションにログインした従業員情報を設定
+                putSessionScope(AttributeConst.LOGIN_EMP, ev);
+                // セッションにログイン完了のフラッシュメッセージを設定
+                putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGGEDIN.getMessage());
+                // トップページへリダイレクト
+                redirect(ForwardConst.ACT_TOP, ForwardConst.CMD_INDEX);
+            }
+        } else {
+            // 認証失敗の場合
+
+            // CSRF対策用トークンを設定
+            putRequestScope(AttributeConst.TOKEN, getTokenId());
+            // 認証失敗のエラーメッセージ表示フラグを設定
+            putRequestScope(AttributeConst.LOGIN_ERR, true);
+            // 入力された社員番号を設定
+            putRequestScope(AttributeConst.EMP_CODE, code);
+
+            // ログイン画面を表示
+            forward(ForwardConst.FW_LOGIN);
+        }
     }
 }
